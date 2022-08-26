75a9a5a354c31f1b7694800f4643e0976feff0ce
Edgardorms <rhedgardo@gmail.com>
2022-08-25 22:20:29 -0300

    agregue responsive

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -12,7 +12,7 @@ function TaskList() {
     );
   }
   return (
-    <div className="grid grid-cols-4 gap-2">
+    <div className="grid grid-cols-1 gap-2  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} />
       ))}
